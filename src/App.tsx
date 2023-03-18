435f6e4518d325327f653552c618a6073668f758
Rzepakson <krzysztof.rzepa95@gmail.com>
2023-03-18 22:25:18 +0100

    Revert "Added few main components and introducing very basic CSS."
    This reverts commit 150536333eeab25ca45d9375241ecd24e2392fdf.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import {Header} from "./Components/layout/Header";
-import {AddShoppingList} from "./Components/AddShoppingList/AddShoppingList";
-
-import "./App.css";
 
 const App = () => {
-    return (
-        <div className="wrapper">
-            <Header/>
-            <AddShoppingList/>
-        </div>
-    );
+  return (
+      <p>Test</p>
+  );
 }
 
 export default App;
